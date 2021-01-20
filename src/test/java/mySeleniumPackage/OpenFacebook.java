9c4143ef20b5bbb07aaf53cce6a5a71837830d94
isratfirdous <isratfirdous@gmail.com>
2021-01-20 19:16:05 +0530

    updated 1

--- src/test/java/mySeleniumPackage/OpenFacebook.java
+++ src/test/java/mySeleniumPackage/OpenFacebook.java
@@ -2,18 +2,23 @@ package mySeleniumPackage;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class OpenFacebook {
 
-	public void testcase() {
-		System.setProperty("webdriver.gecko.driver","/Users/Documents/geckodriver");
-		WebDriver driver = new FirefoxDriver();
-		driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public static  void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\Israt\\selenium setup downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://www.facebook.com");
+        
 		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.xpath("//*[text()='Create New Account']")).click();
 		driver.close();
 	}
 
